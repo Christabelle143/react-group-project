301a53ef3a00e2f80b5c278083fd3223b5e957ba
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-28 17:44:45 +0100

    fix linters errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import MissionsReducer from './missions/fetchMissionData';
+import MissionsReducer from './missions/missions';
 
 const rootReducer = combineReducers({
   missions: MissionsReducer,
