e260dd8c83942a2d16f5cc08d0b8dac2b49fee6e
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-09-28 14:18:40 -0700

    setup my profile component

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,26 +1,28 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import planet from './planet.png';
 
 function Navbar() {
   return (
-    <div className="nav-links">
-      <nav>
-        <div>
-          <img src={planet} alt="logo" />
-        </div>
-        <ul>
-          <li>
-            <NavLink to="/rockets">Rockets</NavLink>
-          </li>
-          <li>
-            <NavLink to="/missions">Missions</NavLink>
-          </li>
-          <li>
-            <NavLink to="/profile">My Profile</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <div className="navbar">
+      <div>
+        <NavLink className="brand" to="/">
+          <img src={planet} alt="logo" className="brand-logo" />
+          <span className="brand-name">Space Travelers' Hub </span>
+        </NavLink>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <NavLink className="nav-item" to="/rockets">Rockets</NavLink>
+        </li>
+        <li>
+          <NavLink className="nav-item" to="/missions">Missions</NavLink>
+        </li>
+        <li>
+          <NavLink className="nav-item" to="/profile">My Profile</NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
