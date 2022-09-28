b1254a62987c24b0a7b072f2952ae1a10ce89563
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-28 14:36:12 +0100

    set up rocket page

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Rockets() {
   return (
     <div className="rockets-container">
-      <h1>My rocket page</h1>
+      <h1>This is the rockets page</h1>
     </div>
   );
 }
