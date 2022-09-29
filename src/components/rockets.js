d6c9a3129635d437550fd6e37724f06542d0548b
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-09-29 12:18:58 -0700

    render rockets to the DOM

--- src/components/rockets.js
+++ src/components/rockets.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import RocketCards from './RocketCards';
+import fetchRocketsData from '../redux/rockets/fetchRockets';
 
-function Rockets() {
+const Rockets = () => {
+  const rocketsData = useSelector((state) => state.rockets.rockets);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!rocketsData.length) {
+      dispatch(fetchRocketsData());
+    }
+  }, []);
+
+  const rocketList = rocketsData.map((rocket) => (
+    <RocketCards
+      key={rocket.id}
+      rocketId={rocket.id}
+      title={rocket.rocket_name}
+      image={rocket.flickr_images[0]}
+      description={rocket.description}
+      isReserved={rocket.reserved}
+    />
+  ));
+
   return (
-    <div className="rockets-container">
-      <h1>This is the rockets page</h1>
+    <div className="Rockets">
+      {rocketList}
     </div>
   );
-}
+};
+
 export default Rockets;
