acd82001adfe11d3e9ee6ebde37b255082b1e786
Christabelle143 <christabelleyouta143@gmail.com>
2022-09-29 09:21:01 +0100

    fetch missions data

---
+++ src/redux/missions/missions.js
@@ -0,0 +1,67 @@
+const BASE_URL = 'https://api.spacexdata.com/v3/missions';
+const LOAD_MISSIONS = 'missions/LOAD_MISSIONS';
+const JOIN_MISSION = 'missions/JOIN_MISSIONS';
+const LEAVE_MISSION = 'missions/LEAVE_MISSION';
+
+export const loadMissions = (payload) => ({
+  type: LOAD_MISSIONS,
+  payload,
+});
+
+export const joinMission = (id) => ({
+  type: JOIN_MISSION,
+  payload: id,
+});
+
+export const leaveMission = (id) => ({
+  type: LEAVE_MISSION,
+  payload: id,
+});
+
+export const fetchMissions = async (dispatch) => {
+  const response = await fetch(BASE_URL);
+  const missions = await response.json();
+
+  dispatch(
+    loadMissions(
+      missions.map((mission) => ({
+        id: mission.mission_id,
+        name: mission.mission_name,
+        description: mission.description,
+      })),
+    ),
+  );
+};
+
+const MissionsReducer = (state = [], action) => {
+  switch (action.type) {
+    case LOAD_MISSIONS:
+      return action.payload;
+    case JOIN_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.id) {
+          return mission;
+        }
+
+        return {
+          ...mission,
+          reserved: true,
+        };
+      });
+    case LEAVE_MISSION:
+      return state.map((mission) => {
+        if (mission.id !== action.id) {
+          return mission;
+        }
+
+        return {
+          ...mission,
+          reserved: false,
+        };
+      });
+    default:
+      return state;
+  }
+};
+
+export default MissionsReducer;
