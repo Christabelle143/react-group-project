19d8e273f6bbf00016bbcefcdc66b873dcabbae5
Suleiman Olaide <olaide.suleiman96@gmail.com>
2022-09-30 14:10:18 -0700

    make changes

--- src/_test_/missions.test.js
+++ src/_test_/missions.test.js
@@ -1,7 +1,8 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
-import store from '../redux/configurestore';
+import store from '../redux/configurestore.js';
 import Missions from '../components/missions';
 
 it('Renders Rockets component correctly', () => {
--- src/_test_/profile.test.js
+++ src/_test_/profile.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/extensions */
 import React from 'react';
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
